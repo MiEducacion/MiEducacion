eee9a2c125ce7e3d6098d7aeca50a40045d13e46
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-04-13 01:09:58 -0300

    [frontend] Helper that extracts client settings

---
+++ app/javascript/packs/mieducacion/helpers/index.js
@@ -0,0 +1,6 @@
+// Extract the values ​​from window.SiteSettings to use inside the frontend instance
+const SiteSettings = window.MiEducacion.SiteSettings || {}
+
+module.exports = {
+    SiteSettings
+}
