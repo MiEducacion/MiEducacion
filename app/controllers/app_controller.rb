7d1aaec9a6e0cddf66dfbb2aca3ea568f1402d50
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-04-23 22:10:26 -0300

    Create "Public Site" setting
    Public access will allow anonymous read-only access to certain content on the site (crawlers included)

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,10 @@
 class AppController < ApplicationController
-  def index
+layout 'mieducacion_web'
+  before_action :authenticate_user!, unless: -> { SiteSettings.public_site == true}
+  def home
+  end
+
+  def app
+    
   end
 end
--- app/models/site_settings.rb
+++ app/models/site_settings.rb
@@ -7,6 +7,7 @@ class SiteSettings < RailsSettings::Base
   # field :host, type: :string, default: "http://localhost:3000"
   field :site_title, default: ( ENV["MIEDUCACION_SITE_TITLE"] ||"MiEducación"), validates: { presence: true, length: { in: 2..255 } }
   field :default_locale, default: "en", type: :string
+  field :public_site, default: ( ENV["MIEDUCACION_PUBLIC_SITE"] ||"0"), type: :boolean
   # field :confirmable_enable, default: "0", type: :boolean
   field :developer_emails, default: (ENV["MIEDUCACION_DEVELOPER_EMAILS"] || "developer@mieducacion.net"), type: :array, readonly: true
   # field :omniauth_google_client_id, default: (ENV["OMNIAUTH_GOOGLE_CLIENT_ID"] || ""), type: :string, readonly: true
