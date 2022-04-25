170d1b1f4208d86a2760a626bf04acf756ac297c
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-04-25 10:57:19 -0300

    Expose SiteLogo in client_config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,7 @@ module ApplicationHelper
       def client_side_app_settings
         site_settings = {
           title: SiteSettings.site_title,
+          site_logo: SiteSettings.site_logo,
           public_site: SiteSettings.public_site,
           show_site_banner: SiteSettings.show_site_banner,
           site_banner_content: SiteSettings.site_banner_content
