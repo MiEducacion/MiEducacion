55153f7eee59e6192d9d16a1478c5275984285ee
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-04-25 10:56:10 -0300

    Show "Login Required" when force_redirect is false

--- app/javascript/packs/mieducacion/routes/index.js
+++ app/javascript/packs/mieducacion/routes/index.js
@@ -5,55 +5,74 @@ import Meta from 'vue-meta'
 
 import Home from "../views/home";
 import NotFound from '../views/errors/404'
+import LoginRequired from '../views/errors/LoginRequired'
+
 const Courses = () => import(/* webpackChunkName: "site-courses" */ '../views/courses')
-const UserLogin = () =>
-  import(/* webpackChunkName: "user-login" */ '../views/auth/Login')
+// const UserLogin = () =>
+  // import(/* webpackChunkName: "user-login" */ '../views/auth/Login')
 
 Vue.use(VueRouter)
 Vue.use(Meta)
 
 const routes = [
-    { path: '/', name: "home", component: Home, alias: '/home'},
-    {
-        path: '/courses',
-        name: 'Courses',
-        component: Courses
+  { path: '/', name: "home", component: Home, alias: '/home' },
+  {
+    path: '/courses',
+    name: 'Courses',
+    component: Courses
 
-    },
-    {
-        path: '/my',
-        redirect: '/my/summary'
-    },
-    {
-        path: '/my/summary',
-        name: 'MySummary'
-    },
-    {
-        path: '/my/profile',
-        name: 'MyProfile'
-    },
-    {
-        path: '*',
-        name: 'NotFound',
-        errorCode: 404,
-        component: NotFound,
-        meta: {
-          layout: 'MinimalLayout'
-        }
-    },
+  },
+  {
+    path: '/my',
+    redirect: '/my/summary'
+  },
+  {
+    path: '/my/summary',
+    name: 'MySummary'
+  },
+  {
+    path: '/my/profile',
+    name: 'MyProfile'
+  },
+  {
+    path: '/login-required',
+    name: 'LoginRequired',
+    component: LoginRequired,
+    meta: {
+      layout: 'MinimalLayout'
+    }
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    errorCode: 404,
+    component: NotFound,
+    meta: {
+      layout: 'MinimalLayout'
+    }
+  },
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    routes
+  mode: "history",
+  routes
 });
 
-router.beforeResolve((to, from, next) => {
-    if (window.MiniProfiler !== undefined) {
-        window.MiniProfiler.pageTransition();
-        next()
-      }
-    else next()
-  })
+router.beforeEach((to, from, next) => {
+  if (window.MiniProfiler !== undefined) {
+    window.MiniProfiler.pageTransition();
+
+    if (window.MiEducacion.SiteSettings.public_site == false && to.path !== '/login-required') {
+      next( { path: '/login-required' })
+    }
+  else next()
+  }
+
+  if (window.MiEducacion.SiteSettings.public_site == false && to.path !== '/login-required') {
+    next( { path: '/login-required' })
+  }
+
+  else next()
+})
 
 export default router;
