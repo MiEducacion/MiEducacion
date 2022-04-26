c4dd824de29c48729c875cbac2051e11c9d9e5fe
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-04-26 20:41:11 -0300

    [JSBeautified] Show 404 when route requires auth
    We are considering showing different views based on the Path, for example: "You must be logged in to change your preferences" when accessing "/my/preferences"

--- app/javascript/packs/mieducacion/routes/index.js
+++ app/javascript/packs/mieducacion/routes/index.js
@@ -11,74 +11,82 @@ import NotFound from '../views/errors/404'
 import LoginRequired from '../views/errors/LoginRequired'
 
 const Courses = () => import( /* webpackChunkName: "site-courses" */ '../views/courses')
+const MyProfile = () => import( /* webpackChunkName: "my-profile" */ '../views/users/my')
+
 
 Vue.use(VueRouter)
 Vue.use(Meta)
 
 const routes = [{
-		path: '/',
-		name: "home",
-		component: Home,
-		alias: '/home'
-	},
-	{
-		path: '/courses',
-		name: 'Courses',
-		component: Courses
+        path: '/',
+        name: "home",
+        component: Home,
+        alias: '/home'
+    },
+    {
+        path: '/courses',
+        name: 'Courses',
+        component: Courses
 
-	},
-	{
-		path: '/my',
-		redirect: '/my/summary'
-	},
-	{
-		path: '/my/summary',
-		name: 'MySummary'
-	},
-	{
-		path: '/my/profile',
-		name: 'MyProfile'
-	},
-	{
-		path: '/login-required',
-		name: 'LoginRequired',
-		component: LoginRequired,
-		meta: {
-			layout: 'MinimalLayout'
-		}
-	},
-	{
-		path: '*',
-		name: 'NotFound',
-		errorCode: 404,
-		component: NotFound,
-		meta: {
-			layout: 'MinimalLayout'
-		}
-	},
+    },
+    {
+        path: '/my',
+        redirect: '/my/profile'
+    },
+    {
+        path: '/my/profile',
+        name: 'MyProfile',
+        component: MyProfile,
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/login-required',
+        name: 'LoginRequired',
+        component: LoginRequired,
+        meta: {
+            layout: 'MinimalLayout'
+        }
+    },
+    {
+        path: '*',
+        name: 'NotFound',
+        errorCode: 404,
+        component: NotFound,
+        meta: {
+            layout: 'MinimalLayout'
+        }
+    },
 ];
 
 const router = new VueRouter({
-	mode: "history",
-	routes
+    mode: "history",
+    routes
 });
 
+
 router.beforeEach((to, from, next) => {
-	if (window.MiniProfiler !== undefined) {
-		window.MiniProfiler.pageTransition();
+    if (SiteSettings.public_site == false && !currentUser && to.path !== '/login-required') {
+        next({
+            path: '/login-required'
+        })
+    }
+    else if (to.meta.requiresAuth && !currentUser) {
+        next({
+            name: 'NotFound',
+            params: [to.path],
+            replace: true
+        })
 
-		if (SiteSettings.public_site == false && !currentUser && to.path !== '/login-required') {
-			next({
-				path: '/login-required'
-			})
-		} else next()
-	}
+    }
+    else next()
+})
 
-	if (SiteSettings.public_site == false && !currentUser && to.path !== '/login-required') {
-		next({
-			path: '/login-required'
-		})
-	} else next()
+router.afterEach((to, from) => {
+    if (window.MiniProfiler && from) {
+        window.MiniProfiler.pageTransition();
+    }
 })
 
 export default router;
