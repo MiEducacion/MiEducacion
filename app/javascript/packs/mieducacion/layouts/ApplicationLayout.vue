4adb65a8ebb00827c11aacf6f4eeb8823a1329a3
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-04-27 17:40:43 -0300

    Create Bottom Navigation on Mobile

--- app/javascript/packs/mieducacion/layouts/ApplicationLayout.vue
+++ app/javascript/packs/mieducacion/layouts/ApplicationLayout.vue
@@ -1,15 +1,21 @@
 <template>
+<div>
 <v-main id="main-mountpoint">
   <Header />
   <router-view />
 </v-main>
+<SiteBottomNavigation />
+</div>
 </template>
 
 <script>
 import Header from '../partials/_header.vue'
+import SiteBottomNavigation from '../partials/_bottom-navigation.vue'
+
 export default {
   components: {
     Header,
+    SiteBottomNavigation
   },
 }
 </script>
---
+++ app/javascript/packs/mieducacion/partials/_bottom-navigation.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-bottom-navigation
+    v-if="isMobile"
+    v-model="navSelected"
+    background-color="#fff"
+    color="#4527a0"
+    shift
+    grow
+    fixed
+  >
+    <v-btn to="/">
+      <span>Inicio</span>
+
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn to="/courses">
+      <span>Cursos</span>
+
+      <v-icon>mdi-school-outline</v-icon>
+    </v-btn>
+
+    <v-btn to="/groups">
+      <span>Grupos</span>
+
+      <v-icon>mdi-account-group-outline</v-icon>
+    </v-btn>
+
+    <v-btn to="/resources">
+      <span>Recursos</span>
+
+      <v-icon>mdi-checkbox-multiple-blank-outline</v-icon>
+    </v-btn>
+
+    <v-btn to="/grades" v-if="currentUser">
+      <span>Calificaciones</span>
+
+      <v-icon>mdi-star-outline</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+    name: 'SiteBottomNavigation',
+    data () {
+        return {
+            isMobile: window.MiEducacion.mobile,
+            currentUser: window.MiEducacion.currentUser
+        }
+         
+         },
+
+    computed: {
+
+    },
+  }
+
+</script>
+
+<style>
+
+</style>
