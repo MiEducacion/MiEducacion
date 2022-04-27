07dfe0260efcb615bcc7a391dd04cafbc09b2ea6
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-04-27 17:45:07 -0300

    Add "No Courses" view

--- app/javascript/packs/mieducacion/views/courses/index.vue
+++ app/javascript/packs/mieducacion/views/courses/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="site-courses" class="pt-10">
    <site-spinner v-if="!CoursesData"/>
-    <v-container v-if="CoursesData" fluid class="fill-height courses-container">
-      
+    <v-container v-if="CoursesData" fluid class="fill-height courses-container align-start">
+      <div class="courses--no-data mx-auto text-center" v-if="!CoursesData.lenght">
+        <img :src="NoDataImage" class="illustration"/>
+        <h1 class="NoCoursesTitle">
+          Actualmente no estás inscripto en ninguna clase.
+        </h1>
+      </div>
     </v-container>
   </div>
 </template>
 
 <script>
+import NoDataImage from 'images/illustrations/no_courses.png'
+
 export default {
   metaInfo: {
     title: 'Cursos',
@@ -17,6 +24,7 @@ export default {
       loading: null,
       error: null,
       CoursesData: null,
+      NoDataImage
     }
   },
   methods: {
