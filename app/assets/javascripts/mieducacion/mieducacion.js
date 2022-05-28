0a1a10c7fa1d02266cfd01f13bdc9f138a8b3b25
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-28 17:52:06 -0300

    [DEV WIP] Register components that are in the head tag

--- app/assets/javascripts/mieducacion/mieducacion.js
+++ app/assets/javascripts/mieducacion/mieducacion.js
@@ -51,6 +51,19 @@ Vue.prototype.currentUser = window.MiEducacion.currentUser;
 Vue.prototype.SiteSettings = window.MiEducacion.SiteSettings;
 Vue.prototype.isMobile = window.MiEducacion.mobile;
 
+/* TODO: Register the components that are in head tag so that they are available in the app */
+$('script[type="text/x-template"]').each(function () {
+    let $this = $(this);
+    let name = $this.attr("name") || $this.data("template-name");
+
+    if (window.console) {
+      window.console.log(
+        "Detected x-template named: " + name
+      );
+    }
+    /* At the moment we eliminate them until we discover the method to register them */
+    $this.remove();
+  });
 
 
 document.addEventListener('DOMContentLoaded', () => {
