41b71996e0784a72d2eec20a27ccb7ff60e147b4
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-29 20:13:52 -0300

    [DEV] Fix deprecation: Initialization autoloaded the constant SiteSettings
    DEPRECATION WARNING: Initialization autoloaded the constant SiteSettings.
    
    Being able to do this is deprecated. Autoloading during initialization is going
    to be an error condition in future versions of Rails.
    
    Reloading does not reboot the application, and therefore code executed during
    initialization does not run again. So, if you reload SiteSettings, for example,
    the expected changes won't be reflected in that stale Class object.
    
    This autoloaded constant has been unloaded.
    
    In order to autoload safely at boot time, please wrap your code in a reloader
    callback this way:
    
        Rails.application.reloader.to_prepare do
          # Autoload classes and modules needed at boot time here.
        end
    
    That block runs when the application boots, and every time there is a reload.
    For historical reasons, it may run twice, so it has to be idempotent.
    
    Check the "Autoloading and Reloading Constants" guide to learn more about how
    Rails autoloads and reloads.
     (called from <main> at C:/Users/alexb/Documents/Proyectos/MiEducacion_Rails/MiEducacion/config/environment.rb:5)

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,3 +1,5 @@
-Rails.application.configure do
-    config.force_ssl = SiteSettings.force_https
+Rails.application.reloader.to_prepare do
+    Rails.application.configure do
+        config.force_ssl = SiteSettings.force_https
+    end
 end
