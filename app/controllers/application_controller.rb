9eb045ee49e959ddc91de57bd5a994b9863f442e
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-29 00:28:09 -0300

    [fix] Revert change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-    before_action do
-        if current_user && current_user.has_role?(:admin)
-          Rack::MiniProfiler.authorize_request
-        end
-      end
 end
