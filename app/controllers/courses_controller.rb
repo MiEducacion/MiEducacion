4a4347dd15bc220471238b106634fa72e2f0e6d2
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-31 18:50:28 -0300

    Update courses_controller.rb

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,70 +1,62 @@
 # frozen_string_literal: true
 
 class CoursesController < ApplicationController
-  layout 'application'
-  require 'action_view'
+  layout "application"
+  require "action_view"
   include ActionView::Helpers::DateHelper
 
   before_action :public_site?
 
   def index
+    @courses = Course.all
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: {
+                 courses: @courses,
 
-  @courses = Course.all
+               },
+               content_type: "application/json"
+      }
+    end
+  end
 
-  respond_to do |format| 
-          format.html
-          format.json {         
-              render json: {
-              courses: @courses
-              
-          },
-          content_type: 'application/json' }
-  end 
-end
+  def getCourse
+    @course = Course.find(params[:id])
 
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: {
+                 course: @course,
 
-def getCourse
-
-  @course = Course.find(params[:id])
-
-  respond_to do |format| 
-          format.html
-          format.json {         
-              render json: {
-              course: @course
-              
-          },
-          content_type: 'application/json' }
-  end 
-end
-
-
-
+               },
+               content_type: "application/json"
+      }
+    end
+  end
 
   def newCourse
-
     @course = Course.new({
       :name => params[:name],
       :teachers => params[:teachers],
-      :private => params[:private]
+      :private => params[:private],
     })
-    
+
     @course.save
-  
-    respond_to do |format| 
-            format.json {         
-                render json: {
-                  course: @course
-                  }, content_type: 'application/json', status: 200 }
-          
-    end 
 
-end
+    respond_to do |format|
+      format.json {
+        render json: {
+                 course: @course,
+               }, content_type: "application/json", status: 200
+      }
+    end
+  end
 
-private
-   
-def public_site?
-     redirect_to "/login_required", status: 301 unless SiteSettings.public_site || current_user
-end
+  private
 
+  def public_site?
+    redirect_to "/login_required", status: 301 unless SiteSettings.public_site || current_user
+  end
 end
