0e78d07c73325aa6526747c15f4d9a7ed06c878c
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-30 21:44:07 -0300

    Add Anon User Banner

---
+++ app/assets/javascripts/mieducacion/components/anon-user-banner.vue
@@ -0,0 +1,28 @@
+<template>
+   <v-banner
+      color="var(--mieducacion-primary)"
+      dark
+      v-if="!currentUser"
+      v-emoji
+      app
+      single-line
+      icon=""
+      elevation="0"
+      >
+      You are browsing the site as an anonymous user, register to take full advantage 😁
+      <template v-slot:actions="{ dismiss }">
+         <v-btn
+            text
+            dark
+            @click="dismiss"
+            >
+            Dismiss
+         </v-btn>
+      </template>
+   </v-banner>
+</template>
+<script>
+   export default {
+       name: 'AnonUserBanner'
+   }
+</script>
--- app/assets/javascripts/mieducacion/layouts/ApplicationLayout.vue
+++ app/assets/javascripts/mieducacion/layouts/ApplicationLayout.vue
@@ -2,6 +2,7 @@
 <div>
 <v-main id="main-mountpoint">
   <SiteHeader />
+  <AnonUserBanner/>
   <slot name="below-site-header" />
   <router-view />
 </v-main>
@@ -12,11 +13,13 @@
 <script>
 import SiteHeader from '../components/site-header.vue'
 import SiteBottomNavigation from '../components/site-bottom-navigation.vue'
+import AnonUserBanner from '../components/anon-user-banner.vue'
 
 export default {
   components: {
     SiteHeader,
-    SiteBottomNavigation
+    SiteBottomNavigation,
+    AnonUserBanner
   },
 }
 </script>
