32c9d2f74eb42b7066ee12632e7233a5a506bbd4
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-30 21:27:18 -0300

    Add Courses card component

--- app/assets/javascripts/mieducacion/views/courses/CoursesHome.vue
+++ app/assets/javascripts/mieducacion/views/courses/CoursesHome.vue
@@ -1,81 +1,85 @@
 <template>
-  <div class="courses-discover fill-height">
-    <SiteSpinner v-if="!CoursesData" />
-    <v-container
-      v-if="CoursesData"
-      fluid
-      class="fill-height courses-container align-start"
-    >
-      <v-col class="py-0 px-0">
-        <div class="my-courses">
-        <h2 class="title">My Courses</h2>
-        <div class="courses--no-data mx-auto text-center">
-          <img :src="NoDataImage" class="illustration" />
-          <h1 class="title">
-            Actualmente no estás inscripto en ninguna clase.
-          </h1>
-        </div>
-        </div>
-        <div class="explore-courses">
-          <h2 class="title">Explore courses</h2>
-          <p v-if="CoursesData">{{CoursesData.courses}}</p>
-        </div>
-      </v-col>
-    </v-container>
-    <v-btn
-      v-if="currentUser && currentUser.is_admin"
-      @click="showCreateCourses = true"
-      dark
-      right
-      bottom
-      fixed
-      class="courses extended-fab"
-      :class="{'mobile' : isMobile}"
-      fab
-    >
-      <v-icon left>mdi-plus</v-icon>
-      Crear curso
-    </v-btn>
-    <create-course v-model="showCreateCourses" />
-  </div>
+   <div class="courses-discover fill-height">
+      <SiteSpinner v-if="!CoursesData" />
+      <v-container
+         v-if="CoursesData"
+         fluid
+         class="fill-height courses-container align-start"
+         >
+         <v-col class="py-0 px-0">
+            <div class="my-courses pb-4" v-if="currentUser">
+               <h2 class="title">My Courses</h2>
+               <div class="courses--no-data mx-auto text-center">
+                  <img :src="NoDataImage" class="illustration" />
+                  <h1 class="title">
+                     Actualmente no estás inscripto en ninguna clase.
+                  </h1>
+               </div>
+            </div>
+            <div class="explore-courses pt-12">
+               <h2 class="title">Explore courses</h2>
+               <p v-if="CoursesData">{{CoursesData.courses}}</p>
+               <v-row class="courses-grid">
+                  <template v-for="course in CoursesData">
+                     <course-card :key="course.id" :courseData="course"></course-card>
+                  </template>
+               </v-row>
+            </div>
+         </v-col>
+      </v-container>
+      <v-btn
+         v-if="currentUser && currentUser.is_admin"
+         @click="showCreateCourses = true"
+         dark
+         right
+         bottom
+         fixed
+         class="courses extended-fab"
+         :class="{'mobile' : isMobile}"
+         fab
+         >
+         <v-icon left>mdi-plus</v-icon>
+         Crear curso
+      </v-btn>
+      <create-course v-model="showCreateCourses" />
+   </div>
 </template>
-
 <script>
-import NoDataImage from 'images/illustrations/no_courses.png'
-import CreateCourse from '../../components/modals/courses/CreateCourse.vue'
-
-export default {
-  components: { CreateCourse },
-  metaInfo: {
-    title: 'Cursos',
-  },
-  data() {
-    return {
-      loading: null,
-      error: null,
-      CoursesData: null,
-      NoDataImage,
-      showCreateCourses : false
-    }
-  },
-  methods: {
-    fetchCourses() {
-      $.ajax('/courses.json')
-        .then((response) => {
-          this.CoursesData = response
-          console.log(this.CoursesData)
-        })
-        .catch((error) => {
-          console.log(error)
-          this.error = true
-        })
-    },
-  },
-
-  mounted() {
-    this.fetchCourses()
-  },
-}
+   import NoDataImage from 'images/illustrations/no_courses.png'
+   import CreateCourse from '../../components/modals/courses/CreateCourse.vue'
+   import CourseCard from './components/CourseCard.vue'
+   
+   export default {
+     components: { CreateCourse, CourseCard },
+     metaInfo: {
+       title: 'Cursos',
+     },
+     data() {
+       return {
+         loading: null,
+         error: null,
+         CoursesData: null,
+         NoDataImage,
+         showCreateCourses : false
+       }
+     },
+     methods: {
+       fetchCourses() {
+         $.ajax('/courses.json')
+           .then((response) => {
+             this.CoursesData = response.courses
+             console.log(this.CoursesData)
+           })
+           .catch((error) => {
+             console.log(error)
+             this.error = true
+           })
+       },
+     },
+   
+     mounted() {
+       this.fetchCourses()
+     },
+   }
 </script>
-
 <style></style>
---
+++ app/assets/javascripts/mieducacion/views/courses/components/CourseCard.vue
@@ -0,0 +1,23 @@
+<template>
+   <v-col md="4" sm="6" cols="12" class="py-0">
+      <router-link tag="div" v-ripple class="course-card" v-if="!courseData.private" :to="'/courses/' + courseData.id">
+         <v-card
+            class="mx-auto my-6"
+            >
+            <v-img
+               height="10rem"
+               src="/images/default/course-default.svg"
+               ></v-img>
+            <v-card-title class="course-name">{{courseData.name}}</v-card-title>
+         </v-card>
+      </router-link>
+   </v-col>
+</template>
+<script>
+   export default {
+       name: 'course-card',
+       props: ['courseData']
+   }
+</script>
+<style>
+</style>
