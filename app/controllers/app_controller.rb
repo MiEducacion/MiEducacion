b429f8ea501975324c7ac0eebcf9328f486f0f7e
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-31 16:22:36 -0300

    # frozen_string_literal: true

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AppController < ApplicationController
 layout 'application'
   before_action :authenticate_user!, unless: -> { SiteSettings.public_site == true || SiteSettings.force_redirect_private == false}
