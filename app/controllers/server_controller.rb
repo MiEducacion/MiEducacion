b943cb955823660b86c13745ed89b090f95ecea9
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-06-12 16:10:03 -0300

    [dev] Create Server status controller

---
+++ app/controllers/server_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ServerController < ApplicationController
+  layout false
+  include ActionView::Helpers::DateHelper
+
+  def status
+    render json: server_status.to_json, content_type: "application/json"
+  end
+
+  private
+
+  def server_status
+    {
+      booted_at: MiEducacion::BOOTED_AT.to_s,
+      uptime: time_ago_in_words(MiEducacion::BOOTED_AT),
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,16 @@ Rails.application.routes.draw do
   root to: "app#index", as: "app"
   resources :wizard
   get "session/current", to: "session/current_user#current_session", constraints: lambda { |request|
-                                                                                    request.format == :json
-                                                                                  }
+                       request.format == :json
+                     }
   get "manifest.webmanifest" => "metadata#webmanifest", as: :manifest
   get "manifest.json" => "metadata#webmanifest"
 
   get "courses" => "courses#index"
   post "courses/new" => "courses#new_course"
   get "courses/:id" => "courses#course"
+
+  get "server/status" => "server#status"
 
   authenticated :user, ->(u) { u.has_role?(:admin) } do
     mount Logster::Web => "/logs"
