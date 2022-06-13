7dbef8aa21be88489592254ea9170ef020261cf8
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-06-13 10:18:49 -0300

    Initial admin dashboard

--- app/assets/javascripts/mieducacion/views/admin/AdminDashboard.vue
+++ app/assets/javascripts/mieducacion/views/admin/AdminDashboard.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="panel">
-      <SiteSpinner />
+    <SiteSpinner v-if="!dashboardData" />
+    <section v-else class="version-checks pt-6">
+      <div class="section-title">
+        <h2>Version</h2>
+      </div>
+      <div class="dashboard-stats version-check normal">
+        <div class="version-number">
+          <h4>Installed</h4>
+          <h3>
+            {{dashboardData.version_check.installed_version}}
+          </h3>
+          <div class="sha-link">
+            (
+            <a
+              :href="`https://github.com/MiEducacion/MiEducacion/commits/${dashboardData.version_check.installed_sha}`"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {{ dashboardData.version_check.installed_sha | truncate(10)}}
+            </a>
+            )
+          </div>
+        </div>
+        <div class="version-status">
+          <div class="face">
+            <span class="up-to-date">
+              <v-icon size="3rem">mdi-emoticon-happy-outline</v-icon>
+            </span>
+          </div>
+          <div class="version-notes">You're up to date!</div>
+        </div>
+        <div class="upgrade-header">
+          <a href="#">Updgrade instance</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -13,8 +48,23 @@ export default {
     methods: {
         fetchDashboard() {
             $.ajax('/admin/dashboard.json')
+            .then((response) => {
+             this.dashboardData = response
+           })
+           .catch((error) => {
+             console.log(error)
+             this.error = true
+           })
+
+        }
+    },
+    mounted () {
+        this.fetchDashboard()
+    },
+    data () {
+        return {
+            dashboardData: null
         }
     }
-
 }
 </script>
