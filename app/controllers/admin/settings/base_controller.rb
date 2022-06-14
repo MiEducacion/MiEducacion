6c566edb455d64cf0b6d41f4b5c823e00ce27fb0
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-06-13 22:04:15 -0300

    [wip] Add Controllers and Paths for Settings

--- app/controllers/admin/settings/base_controller.rb
+++ app/controllers/admin/settings/base_controller.rb
@@ -1,34 +1,32 @@
 # frozen_string_literal: true
 
 class Admin::Settings::BaseController < ApplicationController
-    before_action :authenticate_user!
-    before_action :admin?
-    def create
-        result = upsert_config(settings_params)
+  before_action :authenticate_user!
+  before_action :admin?
 
-        if result.success?
-          render json: { message: I18nJS.t("core.success_settings") }, status: :ok
-        else
-          render json: { error: result.errors.to_sentence }, status: :unprocessable_entity
-        end
-      end
+  def create
+    result = upsert_config(settings_params)
 
-      private
-
-      def upsert_config(settings)
-        ::Settings::Upsert.call(settings, authorization_resource)
-      end
-
-
-      def settings_params
-        params
-          .require(:"settings_#{authorization_resource.name.demodulize.underscore}")
-          .permit(*authorization_resource.keys)
-      end
-
-      def admin?
-        render :status => 404 unless current_user.has_role?(:admin)
-      end
+    if result.success?
+      render json: { message: I18nJS.t("core.success_settings") }, status: :ok
+    else
+      render json: { error: result.errors.to_sentence }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def upsert_config(settings)
+    ::Settings::Upsert.call(settings, authorization_resource)
+  end
+
+  def settings_params
+    params
+      .require(:"settings_#{authorization_resource.name.demodulize.underscore}")
+      .permit(*authorization_resource.keys)
+  end
+
+  def admin?
+    render status: 404 unless current_user.has_role?(:admin)
   end
 end
--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -2,4 +2,9 @@
 
 namespace :admin do
   get "dashboard" => "dashboard#index"
+
+  namespace :settings do
+    resources :general_settings, only: [:create]
+    resources :lms_settings, only: [:create]
+  end
 end
