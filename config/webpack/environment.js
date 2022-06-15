4b7a705c608d11a367e000927bf88aa5f8025684
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-06-15 15:55:59 -0300

    Add alias for webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,8 +2,10 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const aliasConfig = require('./alias')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.config.merge(aliasConfig)
 environment.loaders.prepend('vue', vue)
 
 module.exports = environment
