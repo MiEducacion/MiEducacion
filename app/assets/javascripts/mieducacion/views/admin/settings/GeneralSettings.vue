8140a10ff4610ce0c48ff681591b1fc5a61090e5
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-06-16 10:41:47 -0300

    [WIP] DEV: Render settings based on a "model"
    Settings fields will be created "dynamically" based on "settingsModel"
    
    I still need to discover how to make dynamic v-model of the fields (since they are two different object instances in vue)

--- app/assets/javascripts/mieducacion/views/admin/settings/GeneralSettings.vue
+++ app/assets/javascripts/mieducacion/views/admin/settings/GeneralSettings.vue
@@ -5,14 +5,29 @@
       <v-container>
         <div class="pt-4">
           <v-form @submit.prevent="updateSettings">
-            <v-text-field              
-              v-model="settings.site_name"
-              :value="SiteSettings.title"
-              :rules="[() => !!settings.site_name || 'This field is required']"
-              label="Site name"
+            <template v-for="setting in settingsModel">
+              <div class="setting-container" :key="setting.name">
+                <v-text-field
+              v-if="setting.type == 0"            
+              :v-model="settings[setting.name]"
+              :value="setting.value"
+              :rules="[() => !!settings[setting.name] || 'This field is required']"
+              :label="t(`js.admin.settings.general_settings.${setting.name}`)"
               placeholder="MiEducacion"
               required
             ></v-text-field>
+                {{setting}}
+              </div>
+            </template>
+              <v-btn
+              :loading="btnLoading"
+              color="var(--mieducacion-primary)"
+              dark
+              elevation="0"
+              type="submit"
+            >
+              {{t("js.admin.settings.save")}}
+            </v-btn>
           </v-form>
         </div>
       </v-container>
@@ -25,17 +40,48 @@ export default {
   name: 'AdminGeneralSettings',
   data() {
     return {
-      settings: {
-          site_name: this.SiteSettings.title
-      }
+      settings: {},
+      btnLoading: null,
+      settingsModel: [
+        {
+          name: 'site_name',
+          type: 0,
+          value: this.SiteSettings.title
+        },
+        {
+          name: 'site_description',
+          type: 0,
+          value: this.SiteSettings.site_description
+        },
+        {
+          name: 'site_shortname',
+          type: 0,
+          value: this.SiteSettings.site_shortname
+        },
+        {
+          name: 'public',
+          type: 1,
+          value: this.SiteSettings.public_site
+        }
+      ]
     }
       },
         methods: {
     updateSettings () {
-      $.post('/admin/settings/general_settings', {
-        settings_general: this.settings
+     // this.btnLoading = true
+     // console.log(this.settings)
+      console.log(this.$data)
+/*       $.post('/admin/settings/general_settings', {
+       settings_general: this.settings
       })
-    }
-  }
+      .then((response) => {
+})
+                .catch((error) => {
+                    console.log(error)
+                    this.error = true
+                    this.btnLoading = false
+                }) */
+    },
+  },
 }
 </script>
