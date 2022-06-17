fc07a564848f37d2a78d4be182660f4e158674ad
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-06-16 23:47:31 -0300

    DEV: General Settings
    From this commit it will be possible to make site adjustments from the frontend without having to access the Rails console
    
    It is still something in progress but it is functional

--- app/assets/javascripts/mieducacion/views/admin/settings/GeneralSettings.vue
+++ app/assets/javascripts/mieducacion/views/admin/settings/GeneralSettings.vue
@@ -6,38 +6,45 @@
         <div class="pt-4">
           <v-form @submit.prevent="updateSettings">
             <template v-for="setting in settingsModel">
-              <v-row class="setting-container" :key="setting.name" align="start">
+              <v-row
+                class="setting-container"
+                :key="setting.name"
+                align="start"
+              >
                 <v-col cols="12" sm="6">
-                <span class="setting-title">
-                  {{ t(`js.admin.settings.general_settings.${setting.name}.title`) }}
-                </span>
+                  <span class="setting-title">
+                    {{ t(`js.admin.settings.general_settings.${setting.name}.title`) }}
+                  </span>
                 </v-col>
                 <v-col md="6" sm="12" cols="auto">
-                <v-text-field
-                  v-if="setting.type == 0"
-                  @input.native="settings = $event.target.value"
-                  :value="setting.value"
-                  outlined
-                  dense
-                  class="setting-value"
-                  type="text"
-                  :rules="[() => !!settings[setting.name]|| 'This field is required']"
-                  :label="t(`js.admin.settings.general_settings.${setting.name}.title`)"
-                  placeholder="MiEducacion"
-                  required
-                  hide-details
-                ></v-text-field>
-                <v-checkbox
-                  class="mt-0 setting-value"
-                  v-else-if="setting.type == 1"
-                  v-model="checkbox"
-                  :value="SiteSettings.public_site"
-                  :label="t(`js.admin.settings.general_settings.${setting.name}.description`)"
-                >
-                </v-checkbox>
-                <span v-if="setting.type !== 1" class="setting-description">
-                {{ t(`js.admin.settings.general_settings.${setting.name}.description`) }}
-                </span>
+                  <v-text-field
+                    v-if="setting.type == 0"
+                    @input.native="settings[setting.name] = $event.target.value"
+                    :value="setting.value"
+                    outlined
+                    :v-model="settings[setting.name]"
+                    dense
+                    class="setting-value"
+                    type="text"
+                    :rules="[() => !!settings[setting.name]|| 'This field is required']"
+                    :label="t(`js.admin.settings.general_settings.${setting.name}.title`)"
+                    placeholder="MiEducacion"
+                    required
+                    hide-details
+                  ></v-text-field>
+                  <v-checkbox
+                    v-else-if="setting.type == 1"
+                    class="mt-0 setting-value"
+                    :input-value="setting.value"
+                    :value="setting.value"
+                    @change="settings[setting.name] = $event"
+                    :v-model="settings[setting.name]"
+                    :label="t(`js.admin.settings.general_settings.${setting.name}.description`)"
+                  >
+                  </v-checkbox>
+                  <span v-if="setting.type !== 1" class="setting-description">
+                    {{ t(`js.admin.settings.general_settings.${setting.name}.description`) }}
+                  </span>
                 </v-col>
               </v-row>
             </template>
@@ -58,11 +65,13 @@
 </template>
 
 <script>
+import toast from 'vuetify-toast'
+
 export default {
   name: 'AdminGeneralSettings',
   data() {
     return {
-      settings: [],
+      settings: {},
       btnLoading: null,
       settingsModel: [
         {
@@ -90,20 +99,26 @@ export default {
       },
         methods: {
     updateSettings () {
-     // this.btnLoading = true
-     // console.log(this.settings)
-      console.log(this.$data)
-/*       $.post('/admin/settings/general_settings', {
+      if(!$.isEmptyObject(this.settings)) {
+     this.btnLoading = true
+      $.post('/admin/settings/general_settings.json', {
        settings_general: this.settings
       })
       .then((response) => {
+        toast.show({text: response.message})
+        this.btnLoading = false
 })
-                .catch((error) => {
-                    console.log(error)
-                    this.error = true
-                    this.btnLoading = false
-                }) */
-    },
+      .catch((error) => {
+        this.error = true
+        this.btnLoading = false
+        toast.show({text: t('js.core.generic_error')})
+      })
+    }
+     else
+    {
+      toast.show({text: I18n.t('js.core.error_not_modified')})
+    }
+    }
   },
 }
 </script>
