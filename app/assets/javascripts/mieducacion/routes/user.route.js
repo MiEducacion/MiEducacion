5eabc437c106caba74ec85b633db365673de3782
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-06-18 18:09:26 -0300

    Update user.route.js

--- app/assets/javascripts/mieducacion/routes/user.route.js
+++ app/assets/javascripts/mieducacion/routes/user.route.js
@@ -1,9 +1,27 @@
 const UserPage = () => import( /* webpackChunkName: "mieducacion-user-page" */ '../views/UserPage.vue')
 
 let UserRoute = {
-    name: 'UserPage',
+    name: 'user.redirect',
     path: '/user/:id',
-    component: UserPage
+    redirect: '/u/:id',
+    children: [
+        {
+            name: 'user.overview',
+            path: '/u/:id',
+        },
+        {
+            name: 'user.profile',
+            path: '/u/:id/profile',
+        },
+        {
+            name: 'user.send-invites',
+            path: '/u/:id/send-invites',
+        },
+        {
+            name: 'user.library',
+            path: '/u/:id/library',
+        }
+    ]
  }
 
  
