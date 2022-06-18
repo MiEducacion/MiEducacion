c9399adb6dd99c12dff4cbc909f1a076e20a23ad
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-06-17 21:03:04 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_195355) do
+ActiveRecord::Schema.define(version: 2022_06_17_133207) do
 
   create_table "courses", force: :cascade do |t|
     t.text "name"
@@ -86,6 +86,17 @@ ActiveRecord::Schema.define(version: 2022_06_02_195355) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "new_user", default: true
+    t.datetime "registered_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
