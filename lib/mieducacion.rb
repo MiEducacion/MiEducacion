85064f5b925ce22f39dc90cd26ecce843f77a2e8
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-06-19 23:05:55 -0300

    Update mieducacion.rb

--- lib/mieducacion.rb
+++ lib/mieducacion.rb
@@ -5,16 +5,16 @@ require_dependency "version"
 module MiEducacion
   def self.git_version
     @git_version ||= begin
-      git_cmd = "git rev-parse HEAD"
-      try_git(git_cmd, MiEducacion::Application::Version::FULL)
-    end
+        git_cmd = "git rev-parse HEAD"
+        self.try_git(git_cmd, MiEducacion::Application::Version::FULL)
+      end
   end
 
   def self.git_branch
     @git_branch ||= begin
-      git_cmd = "git rev-parse --abbrev-ref HEAD"
-      try_git(git_cmd, "unknown")
-    end
+        git_cmd = "git rev-parse --abbrev-ref HEAD"
+        self.try_git(git_cmd, "unknown")
+      end
   end
 
   def self.try_git(git_cmd, default_value)
@@ -22,12 +22,21 @@ module MiEducacion
 
     begin
       version_value = `#{git_cmd}`.strip
-    rescue StandardError
+    rescue
       version_value = default_value
     end
 
-    version_value = default_value if version_value.empty?
+    if version_value.empty?
+      version_value = default_value
+    end
 
     version_value
+  end
+
+  def self.full_version
+    @full_version ||= begin
+        git_cmd = 'git describe --dirty --match "v[0-9]*" 2> /dev/null'
+        self.try_git(git_cmd, "unknown")
+      end
   end
 end
