74bd95300acab660c68ba58b84921bd1b39cd7ef
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-08-24 00:29:36 -0300

    DEV: Refactorize report-js-error handler

--- app/assets/javascripts/mieducacion/lib/report-js-error.js
+++ app/assets/javascripts/mieducacion/lib/report-js-error.js
@@ -4,56 +4,48 @@ Logster gem, so they are accessible from "/logs" */
 var lastReport = null;
 
 if (!window.Logster) {
-    window.Logster = {
-        enabled: true
-    };
-}/* 
+	window.Logster = {
+		enabled: true
+	};
+}
 
-Vue.config.errorHandler = function(err, vm, info) {
-    console.error(err)
+function logsterHandler(err, severity) {
+	if (severity === 'error') {
+		console.error(err)
+	} else {
+		console.warn(err)
+	}
 
-    if (lastReport && new Date() - lastReport < 1000 * 60) {
-        return;
-    }
-    if (!Logster.enabled) {
-        return;
-    }
+	if (lastReport && new Date() - lastReport < 1000 * 60) {
+		return;
+	}
+	if (!Logster.enabled) {
+		return;
+	}
 
-    lastReport = new Date();
-    var reportData = {
-        message: err.name,
-        url: window.AppRouter.currentRoute.path,
-        window_location: window.location && (window.location + ""),
-        stacktrace: err.stack,
-        severity: 'error'
-    }
+	lastReport = new Date();
+	var reportData = {
+		message: err.name,
+		url: window.AppRouter.currentRoute.path,
+		window_location: window.location && (window.location + ""),
+		stacktrace: err.stack,
+		severity: 'error'
+	}
 
-    axios.post("/logs/report_js_error",
-        reportData
-    );
+	axios.post("/logs/report_js_error",
+		reportData
+	);
+}
+
+export default {
+	install: (app) => {
+		app.config.errorHandler = function(err, vm, info) {
+			logsterHandler(err, 'error')
+		};
+
+		app.config.warnHandler = function(err, vm, info) {
+			logsterHandler(err, 'warning')
+		};
+
+	},
 };
-
-
-Vue.config.warnHandler = function(err, vm, info) {
-    console.warn(err)
-
-    if (lastReport && new Date() - lastReport < 1000 * 60) {
-        return;
-    }
-    if (!Logster.enabled) {
-        return;
-    }
-
-    lastReport = new Date();
-    var reportData = {
-        message: err.name,
-        url: window.AppRouter.currentRoute.path,
-        window_location: window.location && (window.location + ""),
-        stacktrace: err.stack,
-        severity: 'warning'
-    }
-
-    axios.post("/logs/report_js_error",
-        reportData
-    );
-}; */
