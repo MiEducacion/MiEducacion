1723ef89e99dc5d472877e94aa7004279f970f58
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-08-26 17:24:16 -0300

    Update tailwind.config.js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,14 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  mode: 'jit',
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascripts/**/*.js',
     './app/assets/javascripts/mieducacion/**/**/*.vue',
+    './app/assets/javascripts/mieducacion/views/**/**/*.vue',
+    './app/assets/javascripts/mieducacion/components/**/**/*.vue',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
