67817bc6d84120cdc54b1d5f0bb345376540929b
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-08-27 23:08:37 -0300

    DEV: Enable html for markdown it

--- app/assets/javascripts/mieducacion/lib/markdown-it.js
+++ app/assets/javascripts/mieducacion/lib/markdown-it.js
@@ -1,5 +1,8 @@
 const MarkdownIt = require('markdown-it')
-const MarkdownParser = new MarkdownIt();
+const MarkdownParser = new MarkdownIt({
+    html: true,
+    breaks: true
+});
 
 export default MarkdownParser
 
