97b6853fd9d9f716294feabfcf94f353111031ce
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-09-27 21:19:44 -0300

    DEV: Render custom_stylesheet conditionally
    Custom stylesheet should only be rendered if the setting is enabled

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -11,6 +11,10 @@ class AppController < ApplicationController
   def app; end
 
   def custom_stylesheet
-    render body: Settings::LMS.custom_style, content_type: 'text/css'
+    if Settings::LMS.enable_custom_styles?
+      render body: Settings::LMS.custom_style, content_type: 'text/css'
+    else 
+     render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    end
   end
 end
