c2ae766f284752e66b2d3643d1fe84dba62aba82
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-09-29 22:47:37 -0300

    DEV: Create Server spec

---
+++ spec/requests/server_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe "Servers", type: :request do
+  describe "get information from the server" do
+    it "returns a 200" do
+      get "/server/status.json"
+      expect(response.status).to eq(200)
+    end
+  end
+end
