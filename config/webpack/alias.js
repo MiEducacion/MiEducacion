2b65a772a5628c48da2bb0e4dbd0b0e546b2af9e
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-10-06 21:45:34 -0300

    DEV: Webpack alias

--- config/webpack/alias.js
+++ config/webpack/alias.js
@@ -3,8 +3,9 @@ const path = require('path')
 module.exports = {
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '..', '..', 'app/assets/javascripts/mieducacion/src'),
-      '@views': path.resolve(__dirname, '..', '..', 'app/assets/javascripts/mieducacion/src/views')
+      '@': path.resolve(__dirname,'..', '..', 'app/assets/javascripts/mieducacion/'),
+      'views': path.resolve(__dirname, '..', '..', 'app/assets/javascripts/mieducacion/views/'),
+      'components': path.resolve(__dirname, '..', '..', 'app/assets/javascripts/mieducacion/components/')
     }
   }
 }
