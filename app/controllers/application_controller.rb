6fdcd48d0aca5b4c50288ea428d47c7093130b0f
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-10-07 23:18:19 -0300

    DEV: render_not_found_unless_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+    def render_not_found_unless_admin
+        render template: "exceptions/not_found", layout: "exception", status: 404 unless current_user.has_role?(:admin)
+    end
 end
