36c5101f42b9a60d15ea54833766a2cae81062e1
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-10-10 17:23:40 -0300

    DEV: Add Logster config to Initializer

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -5,5 +5,16 @@ Rails.application.reloader.to_prepare do
     #  config.force_ssl = SiteSettings.force_https
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.default_locale = Settings::General.default_locale
+
+    Logster.config.use_full_hostname = true
+    Logster.config.enable_js_error_reporting = Settings::Developer.enable_report_to_logster
+
+    Logster.config.project_directories = [
+        {
+            path: Rails.root.to_s,
+            url: "https://github.com/MiEducacion/MiEducacion",
+            main_app: true
+        }
+    ]
   end
 end
