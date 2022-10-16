9a7a827d2ecda4b03b79167fe64e9725f65161c6
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-10-16 15:07:15 -0300

    DEV: Move Stylesheets outside js folder

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -3,17 +3,31 @@ const path = require('path')
 const { webpackConfig, merge } = require('shakapacker')
 const vueConfig = require('./rules/vue')
 const customConfig = {
+    optimization: {
+        splitChunks: {
+          // include all types of chunks
+          chunks: 'all',
+        },
+      },
     devServer: {
       client: {
         progress: false
       }
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+        ],
+    },
     resolve: {
-      extensions: ['.css', '.scss'],
       alias: {
         '@': path.resolve(__dirname,'..', '..', 'app/assets/javascripts/mieducacion/'),
         'views': path.resolve(__dirname, '..', '..', 'app/assets/javascripts/mieducacion/views/'),
-        'components': path.resolve(__dirname, '..', '..', 'app/assets/javascripts/mieducacion/components/')
+        'components': path.resolve(__dirname, '..', '..', 'app/assets/javascripts/mieducacion/components/'),
+        'stylesheets': path.resolve(__dirname, '..', '..', 'app/assets/stylesheets/')
       }
     }
   }
