99cd3ee15154589e4ad89a4eab682f4266c97ba6
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-10-16 04:52:11 -0300

    FEATURE: Experimental "additional_head_content"
    This experimental setting allows to add additional content inside the head tag, like x-m-components or others

--- app/models/settings/LMS.rb
+++ app/models/settings/LMS.rb
@@ -9,5 +9,7 @@ module Settings
     setting :enable_custom_styles, type: :boolean, default: false
     setting :custom_style, type: :text
     setting :twemoji_cdn, type: :string, default: 'https://twemoji.maxcdn.com/v/14.0.2/'
+    setting :enable_additional_head_content, type: :boolean, default: false
+    setting :additional_head_content, type: :text
   end
 end
