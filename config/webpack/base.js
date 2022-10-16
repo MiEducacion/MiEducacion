74184b262caf39de50c6710bf2c490be1a15b804
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-10-16 09:55:37 -0300

    FIX: Stylesheets not compiling

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,6 +1,8 @@
 const path = require('path');
-const { webpackConfig, merge } = require('@rails/webpacker')
+const { webpackConfig, merge, loaders } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 
 module.exports = merge({
   module: {
@@ -12,10 +14,11 @@ module.exports = merge({
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin()
   ],
   resolve: {
-    extensions: ['.css', '.vue'],
+    extensions: ['.css', '.scss', '.vue'],
     alias: {
       vue: 'vue/dist/vue.esm-bundler',
       '@': path.resolve(__dirname,'..', '..', 'app/assets/javascripts/mieducacion/'),
