35c8e5b9ff091c7ad78b8d43aad5c7738df78e57
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-02-17 13:02:35 -0300

    Update site-bottom-navigation.vue

--- app/assets/javascripts/mieducacion/components/site-bottom-navigation.vue
+++ app/assets/javascripts/mieducacion/components/site-bottom-navigation.vue
@@ -1,46 +1,49 @@
 <template>
-  <v-bottom-navigation
-    v-if="isMobile"
-    background-color="#fff"
-    color="#4527a0"
-    shift
-    grow
-    fixed
-  >
-    <v-btn to="/">
-      <span>Inicio</span>
+  <div id="site-bottom-nav" class="fixed bottom-0 w-full">
+    <div class="inner-content bg-white shadow-lg py-2 flex w-full">
+      <template v-for="link in BottomLinks">
+        <m-icon-button :title="link.title" class="w-full">
+          <component :is="link.icon" />
+        </m-icon-button>
+      </template>
+    </div>
 
-      <v-icon>mdi-home</v-icon>
-    </v-btn>
-
-    <v-btn to="/courses">
-      <span>Cursos</span>
-
-      <v-icon>mdi-school-outline</v-icon>
-    </v-btn>
-
-    <v-btn to="/groups">
-      <span>Grupos</span>
-
-      <v-icon>mdi-account-group-outline</v-icon>
-    </v-btn>
-
-    <v-btn to="/resources">
-      <span>Recursos</span>
-
-      <v-icon>mdi-checkbox-multiple-blank-outline</v-icon>
-    </v-btn>
-
-    <v-btn to="/grades" v-if="currentUser">
-      <span>Calificaciones</span>
-
-      <v-icon>mdi-star-outline</v-icon>
-    </v-btn>
-  </v-bottom-navigation>
+</div>
 </template>
 
-<script>
-export default {
-    name: 'SiteBottomNavigation',
+<script setup>
+import { getCurrentInstance } from 'vue'
+
+import { MessageCircle, GraduationCap, Users as GroupsIcon, Archive as ResourcesIcon, ClipboardCheck, TreeDeciduous } from 'lucide-vue-next'
+import mIconButton from './m-icon-button.vue';
+const currentUser = getCurrentInstance().appContext.config.globalProperties.currentUser
+const t = I18n.t
+
+const BottomLinks = [
+  {
+    title: t("js.lms.courses"),
+    to: '/courses',
+    icon: GraduationCap,
+    showItem: true
+  },
+  {
+    title: t("js.lms.groups"),
+    to: '/groups',
+    icon: GroupsIcon,
+    showItem: true
+  },
+  {
+    title: t("js.lms.resources"),
+    to: '/resources',
+    icon: ResourcesIcon,
+    showItem: true
+  },
+  {
+    title: t("js.lms.grades"),
+    to: '/grades',
+    icon: ClipboardCheck,
+    showItem: currentUser ? true : false 
   }
+]
+
 </script>
