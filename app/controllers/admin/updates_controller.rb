bd89043188fdede236476ffb9d16f8ec7afdf1cd
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-02-20 00:29:06 -0300

    FIX: Misspelled conditional on UpdatesController

--- app/controllers/admin/updates_controller.rb
+++ app/controllers/admin/updates_controller.rb
@@ -1,47 +1,46 @@
 class Admin::UpdatesController < Admin::BaseController
-    def index
-        respond_to do |format|
-        format.html
-        format.json {
-            render json: updates_response
-        }
+  def index
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: updates_response
       end
     end
-
-    def run_update
-        render json: {
-            errors: [
-                "No updates available"
-            ],
-            error_type: "no_updates_available"
-        } if !MiEducacion::Updater.updates_available?
-
-            render json: {
-                errors: [
-                    "Web updater disabled"
-                ],
-                error_type: "web_update_disabled" 
-            }, status: 403 if !SiteSetting.enable_web_updater
-
-        MiEducacion::Updater.run_update
-        head :ok
-
-    end
-
-    def check_progress
-        progress, output = MiEducacion::Updater.run_update
-        render json: { progress: progress, output: output }
-    end
-
-    private
-
-    def updates_response
-        {
-            versions_diff: MiEducacion::Updater.versions_diff,
-            installed_sha: MiEducacion.git_version,
-            updates_available: MiEducacion::Updater.updates_available?
-        }
-    end
-
   end
-  
+
+  def run_update
+    if !MiEducacion::Updater.updates_available?
+      render json: {
+        errors: [
+          "No updates available"
+        ],
+        error_type: "no_updates_available"
+      }
+    elsif !SiteSetting.enable_web_updater
+      render json: {
+        errors: [
+          "Web updater disabled"
+        ],
+        error_type: "web_update_disabled"
+      }, status: 403
+    else
+      MiEducacion::Updater.run_update
+      head :ok
+    end
+  end
+
+  def check_progress
+    progress, output = MiEducacion::Updater.run_update
+    render json: { progress:, output: }
+  end
+
+  private
+
+  def updates_response
+    {
+      versions_diff: MiEducacion::Updater.versions_diff,
+      installed_sha: MiEducacion.git_version,
+      updates_available: MiEducacion::Updater.updates_available?
+    }
+  end
+end
