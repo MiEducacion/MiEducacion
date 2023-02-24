b540d00e4f1890970a989ff89484dc33e86bacf0
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-02-24 12:47:23 -0300

    DEV: Add admin notice for update

--- app/assets/javascripts/mieducacion/views/admin/AdminDashboard.vue
+++ app/assets/javascripts/mieducacion/views/admin/AdminDashboard.vue
@@ -1,44 +1,47 @@
 <template>
   <div class="panel">
     <m-spinner v-if="!dashboardData" />
-    <section v-else class="version-checks pt-6">
+    <div v-else id="admin-dashboard" class="pt-2">
+      <section id="updates-available" class="inline-flex px-4 py-2 w-full bg-emerald-500 text-white mb-8" v-if="dashboardData.version_check.updates_available">
+        <UploadCloud />
+        <span class="text-base px-4">
+          Hay actualizaciones disponibles
+        </span>
+      </section>
+      <h2 class="text-2xl font-medium mx-auto text-center">
+        Dashboard
+      </h2>
+      <section class="version-checks pt-6">
       <div class="section-title">
         <h2>Version</h2>
       </div>
       <div class="dashboard-stats version-check normal">
         <div class="version-number">
           <h4>Installed</h4>
-          <h3>
-            {{dashboardData.version_check.installed_version}}
-          </h3>
+          <h3>{{ dashboardData.version_check.installed_version }}</h3>
           <div class="sha-link">
-            (
-            <a
-              :href="`https://github.com/MiEducacion/MiEducacion/commits/${dashboardData.version_check.installed_sha}`"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              {{ dashboardData.version_check.installed_sha.substr(0,10) }}
-            </a>
-            )
+            (<a :href="commitUrl" rel="noopener noreferrer" target="_blank">{{ formattedSha }}</a>)
           </div>
         </div>
         <div class="version-status">
           <template v-if="!dashboardData.version_check.updates_available">
-          <div class="face">
-            <span class="up-to-date">
-              <SmileIcon :size="48"/>
-            </span>
-          </div>
-          <div class="version-notes">You're up to date!</div>
+            <div class="face">
+              <span class="up-to-date">
+                <SmileIcon :size="48" />
+              </span>
+            </div>
+            <div class="version-notes">You're up to date!</div>
           </template>
           <template v-else>
-          <div class="face">
-            <span class="updates-available">
-              <SmileIcon :size="48"/>
-            </span>
-          </div>
-          <div class="version-notes">Updates available</div>
+            <div class="face">
+              <span class="updates-available">
+                <SmileIcon :size="48" />
+              </span>
+            </div>
+            <div class="version-notes">
+              <p>Updates available</p>
+            </div>
+            <span class="text-gray-500">{{ dashboardData.version_check.versions_diff }} commits behind</span>
           </template>
         </div>
         <div class="upgrade-header">
@@ -46,35 +49,46 @@
         </div>
       </div>
     </section>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-    name: 'AdminDashboard',
-    metaInfo: {
-        title: 'Admin'
-    },
-    methods: {
-        fetchDashboard() {
-            axios.get('/admin/dashboard.json')
-            .then((response) => {
-             this.dashboardData = response.data
-           })
-           .catch((error) => {
-             console.log(error)
-             this.error = true
-           })
+import { UploadCloud } from 'lucide-vue-next'
+import axios from 'axios'
 
-        }
-    },
-    mounted () {
-        this.fetchDashboard()
-    },
-    data () {
-        return {
-            dashboardData: null
-        }
+export default {
+  name: 'AdminDashboard',
+  metaInfo: {
+    title: 'Admin'
+  },
+  data() {
+    return {
+      dashboardData: null
     }
+  },
+  computed: {
+    commitUrl() {
+      return `https://github.com/MiEducacion/MiEducacion/commits/${this.dashboardData.version_check.installed_sha}`
+    },
+    formattedSha() {
+      return this.dashboardData.version_check.installed_sha.substr(0, 10)
+    }
+  },
+  methods: {
+    async fetchDashboard() {
+      try {
+        const response = await axios.get('/admin/dashboard.json')
+        this.dashboardData = response.data
+      } catch (error) {
+        console.log(error)
+        this.error = true
+      }
+    }
+  },
+  mounted() {
+    this.fetchDashboard()
+  }
 }
 </script>
