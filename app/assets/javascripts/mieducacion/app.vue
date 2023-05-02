f85c73717ef8123674fc9dd01c4984e05e817681
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-05-01 21:25:50 -0300

    DEV: Debug mode notice

--- app/assets/javascripts/mieducacion/app.vue
+++ app/assets/javascripts/mieducacion/app.vue
@@ -12,6 +12,7 @@
       <router-view />
     </main>
     <SiteBottomNavigation v-if="isMobile" />
+    <div id="debug-enabled" v-if="DEBUG_ENABLED" @click="callDebugModal">{{ t('js.core.debug_enabled') }}</div>
 </div>
 </template>
 
@@ -22,6 +23,7 @@ import SiteBanner from './components/site-banner.vue'
 import SiteBottomNavigation from './components/site-bottom-navigation.vue'
 import PluginOutlet from './components/plugin-outlet.vue';
 import { registerComponents } from './lib/register-components'
+import { DEBUG_ENABLED } from './initializers/debug-mode';
 
 export default {
   components: {
@@ -32,7 +34,12 @@ export default {
   },
   data() {
     return {
-
+      DEBUG_ENABLED
+    }
+  },
+  methods: {
+    callDebugModal() {
+      alert('a')
     }
   },
   created() {
